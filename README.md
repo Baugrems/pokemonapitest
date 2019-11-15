b2448aaa9fffdaa6bc2894e6efc7b36e5fc8bd15
Shane Copenhagen <copenhagenmhs@gmail.com>
2019-11-15 01:30:17 +0000

    Add search function and initial pokemon page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pokemonapitest
 testing the pokemon api
+
+A pokemon app that searches ID or Name to populate a page with basic info.
---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.render('home');
+});
+
+app.get("/pokemon/:id/", function(req,res){
+    getPokemon(req.params.id.toLowerCase(),function(err, result){
+        res.render("pokemon", {pokemon: result});
+    });
+});
+
+app.get("/search/", function(req,res){
+    res.render("search");
+});
+
+app.post("/search/", function(req,res){
+    if (!req.body) return res.render("/search/");
+    var pokeID = req.body.pokeID.toLowerCase();
+    getPokemon(pokeID,function(err, result){
+        if(err){res.render("/search/")}
+        res.render("pokemon", {pokemon: result});
+    });
+});
+
+
+function getPokemon(id, callback){
+    url = "https://pokeapi.co/api/v2/pokemon/" + id;
+    request(url, { json: true }, (err, res, body) => {
+    if(err) {
+            return callback(err);
+            }
+    else {
+        callback(null, body);
+    }
+});
+
+
+}
+
+//Listener to turn website on
+app.listen(process.env.PORT);
+console.log("App is listening");
